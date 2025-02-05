e4404a82be37f47a7131b26cea7ed14af35cf0b9
Artiom Porcescu <porchiesku@bk.ru>
2025-02-05 12:42:26 +0200

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Artiom Porcescu on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
